40b24ba06e5cadcd265281f348f17ab66dbe68f1
Kevin Oka <34703734+kevinoka@users.noreply.github.com>
2018-11-23 00:29:16 +0800

    Updating Adoption menu, adding Adoption model class

--- app/src/main/java/com/maddoggo/mydoggoapp/arrayAdapter.java
+++ app/src/main/java/com/maddoggo/mydoggoapp/arrayAdapter.java
@@ -8,20 +8,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.maddoggo.mydoggoapp.Model.Adoption;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
-public class arrayAdapter extends ArrayAdapter<AdoptionCards> {
+public class arrayAdapter extends ArrayAdapter<Adoption> {
 
     Context context;
 
-    public arrayAdapter(Context context, int resourceId, List<AdoptionCards> items) {
+    public arrayAdapter(Context context, int resourceId, List<Adoption> items) {
         super(context, resourceId, items);
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
-        AdoptionCards card_item = getItem(position);
+        Adoption card_item = getItem(position);
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.item, parent, false);
@@ -29,10 +30,12 @@ public class arrayAdapter extends ArrayAdapter<AdoptionCards> {
 
         TextView dog_name = convertView.findViewById(R.id.dog_name);
         ImageView image_adoption = convertView.findViewById(R.id.image_adoption);
-        String url = "https://firebasestorage.googleapis.com/v0/b/mydoggo-project.appspot.com/o/dog1.jpg?alt=media&token=b18c2264-ba92-415e-a80a-c1f7b82c9d1d";
-        dog_name.setText(card_item.getName());
+        //String url = "https://firebasestorage.googleapis.com/v0/b/mydoggo-project.appspot.com/o/dog1.jpg?alt=media&token=b18c2264-ba92-415e-a80a-c1f7b82c9d1d";
+
+        dog_name.setText(card_item.getDogName());
+
         Picasso.with(getContext())
-                .load(url)
+                .load(card_item.getDogPict())
                 .into(image_adoption);
 
         //image_adoption.);
